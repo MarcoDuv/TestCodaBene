214826035af11e9d446f542bb45b6f2cdbcb992f
MarcoD <marco.duvacher@gmail.com>
2022-11-09 12:03:33 +0100

    Add the option to input the gtin to diplay the disired dates. Also add some input errors managing

--- stock_watch/stock_watch_app/urls.py
+++ stock_watch/stock_watch_app/urls.py
@@ -5,5 +5,6 @@ APP_NAME = 'StockWatch'
 
 urlpatterns = [
     path('list/', views.list_products, name="list"),
-    path('insertgtin/', views.insert_gtin, name="insertgtin")
+    path('insertgtin/', views.insert_gtin, name="insertgtin"),
+    path('srchgtin/', views.srch_gtin, name="srchgtin")
 ]
--- stock_watch/stock_watch_app/views.py
+++ stock_watch/stock_watch_app/views.py
@@ -7,6 +7,31 @@ def list_products(request):
     return render(request, 'stock_watch_app/products_list.html')
 
 def insert_gtin(request:HttpRequest):
-    product = Product(gtin = request.POST['gtin'], date = request.POST['date'])
-    product.save()
+    ''' View to insert a new gtin by user '''
+    date = request.POST['date']
+    # Check if the gtin is a number
+    try:
+        new_gtin =  int(request.POST['gtin'])
+    except:
+        new_gtin = None
+
+    #TODO Check if it fit the shape of a gtin
+
+    if (new_gtin is not None) & (date is not None):
+        product = Product(gtin = new_gtin, date = request.POST['date'])
+        product.save()
     return redirect('/stock_watch_app/list/')
+
+def srch_gtin(request:HttpRequest):
+    ''' The view that gets the gtin asked by user '''
+    gtin_asked = request.POST['gtinasked']
+    # Check if the gtin is a number
+    try:
+        gtin_asked = int(request.POST['gtinasked'])
+    except:
+        gtin_asked = None
+
+    # Check if the gtin exists
+    if gtin_asked is not None:
+        gtin_asked = {'product_list': Product.objects.filter(gtin = gtin_asked)}
+    return render(request, 'stock_watch_app/products_list.html', context=gtin_asked)
