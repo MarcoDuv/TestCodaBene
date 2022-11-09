b03012a64b6f38da32989afc9eb296a95663f2a3
MarcoD <marco.duvacher@gmail.com>
2022-11-09 12:19:27 +0100

    Ajust UI

--- stock_watch/stock_watch_app/urls.py
+++ stock_watch/stock_watch_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('list/', views.list_products, name="list"),
     path('insertgtin/', views.insert_gtin, name="insertgtin"),
     path('srchgtin/', views.srch_gtin, name="srchgtin")
+    #TODO add dynamic URL to the search (srchgtin/<int: gtin>/ for ex)
 ]
